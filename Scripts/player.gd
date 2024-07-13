5e56c6f6021f235a744e6bd480b0cc31cabe50d2
Dragoslav Zaric <droidstations@gmail.com>
2024-07-13 19:13:06 +0200

    Add bullet scene.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
 @onready var screensize = get_viewport_rect().size
-
 @export var speed = 150
+@export var cooldown = 0.25
+@export var bullet_scene : PackedScene
+var can_shoot = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
